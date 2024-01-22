08d9342d38b419d2056ce74cf71d2154195b30c7
Manuel <manuelmid99@gmail.com>
2024-01-22 15:15:33 +0100

    other

--- working.js
+++ working.js
@@ -1,8 +1,17 @@
 let o = "Ofinto.ch";
 let f = "Friedli.ch";
+let u = "Others";
 
 function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(o, "Cabinet renders");
+work(u, "Uni with D");
+
+//commants to edit with the terminal : 
+//vim --nameFile
+//press i
+//edit
+//press esc
+//save file :w
+//close editor :q
