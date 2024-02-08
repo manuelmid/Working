5ea48a110f801623bdfdb1a94dce0ec0e57d70cd
Manuel <manuelmid99@gmail.com>
2024-02-08 09:30:33 +0100

    wrk

--- working.js
+++ working.js
@@ -6,7 +6,7 @@ function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(o, "Cabinnet_Scenes_V4");
+work(o, "Cabinnet_Scenes_White_V4 --Render");
 
 //commants to edit with the terminal : 
 //vim --nameFile
