b58011e3982a11193e32348e7cb5eb55c6614d7d
Manuel <manuelmid99@gmail.com>
2024-02-22 14:06:17 +0100

    wrk

--- working.js
+++ working.js
@@ -6,7 +6,7 @@ function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(o, "Cloud --Scenes");
+work(o, "Cloud --Scene_V3");
 
 //commants to edit with the terminal : 
 //vim --nameFile
