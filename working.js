343aabbece2a79cf7c454ad5887fe02025e557bd
Manuel <manuelmid99@gmail.com>
2024-05-02 14:20:19 +0200

    wrk

--- working.js
+++ working.js
@@ -6,7 +6,7 @@ function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(f, "Hundshop SVG --animation");
+work(o, "cloud_wall_design");
 
 //To DO : cloud renders --finish --moreDetail
 
