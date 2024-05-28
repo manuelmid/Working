9ad5e4688254bb9a9a2aa501a82ae363be909fa8
Manuel <manuelmid99@gmail.com>
2024-05-28 11:47:29 +0200

    wrk

--- working.js
+++ working.js
@@ -6,7 +6,7 @@ function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(o, "wood panels --montage");
+work(o, "logo / leds --montage");
 
 //To DO : cloud renders --finish --moreDetail
 
