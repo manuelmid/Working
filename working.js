5711c0fde695e24dbeee0ed600b28b5eee724c9a
Manuel <manuelmid99@gmail.com>
2024-07-18 14:59:54 +0200

    wrk
    q

--- working.js
+++ working.js
@@ -6,7 +6,7 @@ function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(o, "lampe --scene");
+work(o, "lampe --scene_v1");
 
 //To DO : cloud renders --finish --moreDetail
 
