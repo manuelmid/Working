452704f0479ffb3607c8c4a9c92245141b001e72
Manuel <manuelmid99@gmail.com>
2024-09-30 09:58:50 +0200

    wrk

--- working.js
+++ working.js
@@ -1,13 +1,14 @@
 let o = "Ofinto.ch";
 let f = "Friedli.ch";
 let u = "Others";
+z col
 
 function work(where, what){
-    alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
-so, "rise --scene");
+    snew Date() + "Hoy trabaje con: " + where + "en " + what);
+so, "rise -renders");
 
 //To DO : cloud renders --finish --moreDetail
-
+<F12>Úss
 //commants to edit with the terminal : 
 //vim --nameFile
 //press i
