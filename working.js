e5718a078e3d05fdd4e4531f8ffffdb70d11fcaa
Manuel <manuelmid99@gmail.com>
2024-10-02 15:16:31 +0200

    wrk

--- working.js
+++ working.js
@@ -1,11 +1,12 @@
 let o = "Ofinto.ch";
 let f = "Friedli.ch";
 let u = "Others";
-z col
 
 function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
-so, "rise_scene_v2");
+}
+
+work(o, "rise_scene_v2");
 
 //To DO : cloud renders --finish --moreDetail
 //commants to edit with the terminal : 
