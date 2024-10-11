e201960914a6e79000b2f4bd95da00d6ddedf333
Manuel <manuelmid99@gmail.com>
2024-10-11 17:52:24 +0200

    wrk

--- working.js
+++ working.js
@@ -6,7 +6,7 @@ function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(o, "active blackfriday animation");
+work(o, "rise_scene --render");
 
 //To DO : cloud renders --finish --moreDetail
 //commants to edit with the terminal : 
