c9e1e9a263a59ca3961a647eb746a5312c144958
Manuel <manuelmid99@gmail.com>
2025-01-27 14:54:45 +0100

    wrk

--- working.js
+++ working.js
@@ -6,7 +6,7 @@ function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(o, "rise animation");
+work(o, "rise --fixing white color");
 
 //To DO : cloud renders --finish --moreDetail
 //commants to edit with the terminal : 
