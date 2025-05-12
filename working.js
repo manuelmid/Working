9dc4eb82b7779ae8a750d61207ae0d19d4831bf7
Manuel <manuelmid99@gmail.com>
2025-05-12 09:46:57 +0200

    wrk

--- working.js
+++ working.js
@@ -6,7 +6,7 @@ function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(o, "folders");
+work(o, "rise scene");
 
 //To DO : cloud renders --finish --moreDetail
 //commants to edit with the terminal : 
