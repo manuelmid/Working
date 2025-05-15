f66e2a7dc73b8872ee3dac45e39a28e3a82c04ac
Manuel <manuelmid99@gmail.com>
2025-05-15 09:23:03 +0200

    wrk

--- working.js
+++ working.js
@@ -6,7 +6,7 @@ function work(where, what){
     alert(new Date() + "Hoy trabaje con: " + where + "en " + what);
 }
 
-work(o, "rise scene");
+work(o, "export models to raw");
 
 //To DO : cloud renders --finish --moreDetail
 //commants to edit with the terminal : 
